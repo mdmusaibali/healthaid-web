ab1742acd207a8401b725c3e86b72c15533cec14
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2023-02-20 22:49:46 +0530

    Card Ui changed

--- src/components/patient/PatientVisitCard.vue
+++ src/components/patient/PatientVisitCard.vue
@@ -2,30 +2,38 @@
   <v-card class="patient-visit-card">
     <!-- FRONT CARD -->
     <div id="pdf-content">
-      <v-card class="card--front mt-10">
-        <div>
-          <QrcodeVue :value="patient.patient_id" size="120" level="H" />
+      <v-card class="card card--front mt-10">
+        <div class="qr-container">
+          <div class="qr">
+            <QrcodeVue
+              :value="patient.patient_id"
+              class="qr-code"
+              size="95"
+              level="H"
+            />
+          </div>
+          <div class="patient_id pa-0 ma-0">{{ patient.patient_id }}</div>
         </div>
-        <div>
-          <div class="align-self-start mb-3">
+        <div class="patient-info">
+          <div class="align-self-start">
             <p
-              class="ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
+              class="label ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
             >
               Name
             </p>
             <p class="text-h7 ma-0 black--text">{{ patient.name }}</p>
           </div>
-          <div class="align-self-start mb-3">
+          <div class="align-self-start">
             <p
-              class="ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
+              class="label ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
             >
               Date of birth
             </p>
             <p class="text-h7 ma-0 black--text">{{ patient.date_of_birth }}</p>
           </div>
-          <div class="align-self-start mb-3">
+          <div class="align-self-start">
             <p
-              class="ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
+              class="label ma-0 text-h7 grey--text text--darken-1 font-weight-medium"
             >
               Sex
             </p>
@@ -34,24 +42,23 @@
         </div>
       </v-card>
       <!-- BACK CARD -->
-      <v-card class="card--front mt-5">
-        <div>
-          <img :src="logo" height="120" width="120" />
+      <v-card class="card card--back mt-5">
+        <div class="logos">
+          <img :src="logopda" class="logo" />
+          <img :src="logohkes" class="logo logohkes" />
+          <img :src="logomrmc" class="logo" />
         </div>
-        <div>
-          <div class="align-self-start mb-3 d-flex align-center">
-            <v-icon color="black" class="mr-3">mdi-map-marker</v-icon>
-            <p class="black--text ma-0 text-caption">
-              8VG3+435, Sedam Rd, Rajapur, Kalaburagi
-            </p>
-          </div>
-          <div class="align-self-start mb-3 d-flex align-center">
-            <v-icon color="black" class="mr-3">mdi-phone</v-icon>
-            <p class="black--text ma-0 text-caption">9886534480</p>
-          </div>
-          <div class="align-self-start mb-3 d-flex align-center">
-            <v-icon color="black" class="mr-3">mdi-gmail</v-icon>
-            <p class="black--text ma-0 text-caption">healthaid@gmail.com</p>
+        <div class="card-down">
+          <p class="pr-name white--text">Health-aid</p>
+          <div class="hospital-info">
+            <div class="align-self-start mb-3 d-flex align-center">
+              <v-icon color="white" class="mr-2">mdi-phone</v-icon>
+              <p class="white--text ma-0 text-caption">9886534480</p>
+            </div>
+            <div class="align-self-start mb-3 d-flex align-center">
+              <v-icon color="white" class="mr-2">mdi-gmail</v-icon>
+              <p class="white--text ma-0 text-caption">healthaid@gmail.com</p>
+            </div>
           </div>
         </div>
       </v-card>
@@ -76,7 +83,10 @@ export default {
   },
   data() {
     return {
-      logo: require("@/assets/img/logo512.png"),
+      logo: require("@/assets/img/logo1024.png"),
+      logomrmc: require("@/assets/img/mrmc.jpeg"),
+      logohkes: require("@/assets/img/hkes.jpeg"),
+      logopda: require("@/assets/img/pda.jpeg"),
     };
   },
   methods: {
@@ -126,15 +136,96 @@ export default {
   flex-direction: column;
   gap: 1rem;
 } */
-.card--front {
-  display: flex;
-  align-items: center;
-  padding: 1.5rem 1.8rem 1.5rem 1.8rem;
+.card {
   background-color: white;
   width: 25rem;
   height: 13rem;
   border: 1.5px solid black;
-  gap: 2rem;
+  border-radius: 0.8rem;
+  overflow: hidden;
+}
+.card--front {
+  display: grid;
+  grid-template-columns: 42fr 58fr;
+  align-items: center;
+  /* padding: 1.5rem 1.8rem 1.5rem 1.8rem; */
+}
+
+.card--back {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.qr-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  background-color: #011f4b;
+  border-radius: 0 !important;
+}
+.qr {
+  padding: 0.6rem;
+  background-color: #fff;
+  border: 3px solid black;
+}
+.qr-code {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.patient-info {
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  margin-left: 1rem;
+}
+.card-down {
+  background-color: #011f4b;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  padding: 1rem 0 0rem 0;
+}
+.label {
+  font-size: 0.85rem;
+}
+.logos {
+  display: flex;
+  gap: 1rem;
+  height: 100%;
+  align-items: center;
+}
+.logo {
+  height: 60px;
+}
+.logohkes {
+  transform: translateX(3px);
+}
+.patient_id {
+  color: #fff;
+  letter-spacing: 10px;
+  font-weight: 500;
+  margin-top: 0.5rem !important;
+  transform: translateX(5px);
+}
+
+.hospital-info {
+  display: flex;
+  gap: 1rem;
+}
+.pr-name {
+  color: black;
+  text-align: center;
+  font-size: 1.8rem;
+  padding: 0;
+  margin: 0;
 }
 
 @media (max-width: 768px) {
