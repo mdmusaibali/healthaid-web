bbed12c482bda422de2ba0ed0c2c275c2b669859
yaseen5748 <syedyaseen1539@gmail.com>
2023-03-16 21:22:49 +0530

    Update Vue.config.js
    add ssl validator bypass code

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+import { defineConfig } from "@vue/cli-service";
+
+export default defineConfig({
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    node: {
+      global: true,
+      process: true,
+      NODE_TLS_REJECT_UNAUTHORIZED: 0,
+    },
+  },
+});
